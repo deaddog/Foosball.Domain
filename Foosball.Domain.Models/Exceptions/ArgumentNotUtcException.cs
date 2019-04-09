628189a8e98608a2c24b21fe4c1b5375e4c144c8
deaddog <mikkelslsl@gmail.com>
2019-04-09 23:51:15 +0200

    Custom exception ensuring Utc timestamps.

---
+++ Foosball.Domain.Models/Exceptions/ArgumentNotUtcException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Foosball.Domain.Models.Exceptions
+{
+    public class ArgumentNotUtcException : ArgumentException
+    {
+        public ArgumentNotUtcException(string paramName) : base(message: $"{nameof(DateTime)} argument must have {nameof(DateTime.Kind)} = {nameof(DateTimeKind.Utc)}.", paramName: paramName)
+        {
+        }
+    }
+}
