28ea75200e82aa258684b723c7da0c20651f4248
deaddog <mikkelslsl@gmail.com>
2019-04-09 23:09:34 +0200

    Interfaces for Command/Query implementations.

---
+++ Foosball.Domain.Application/IExecutor.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Foosball.Domain.Application
+{
+    public interface IExecutor<in TCommand>
+    {
+        Task ExecuteAsync(TCommand command);
+    }
+}
---
+++ Foosball.Domain.Application/IQuerier.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Foosball.Domain.Application
+{
+    public interface IQuerier<in TQuery, TResponse>
+    {
+        Task<TResponse> QueryAsync(TQuery query);
+    }
+}
