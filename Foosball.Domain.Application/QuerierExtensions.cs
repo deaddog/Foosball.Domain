236a7077d5397c49e19a75577084beeea86ffeba
deaddog <mikkelslsl@gmail.com>
2019-04-09 23:19:52 +0200

    Extensions for the two queries.

---
+++ Foosball.Domain.Application/QuerierExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Foosball.Domain.Application.Queries;
+using Foosball.Domain.Models;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace Foosball.Domain.Application
+{
+    public static class QuerierExtensions
+    {
+        public static Task<IImmutableList<TResponse>> GetAsync<TQuery, TResponse>(this IQuerier<Get<TQuery>, IImmutableList<TResponse>> queryHandler, params Id<TQuery>[] ids)
+        {
+            return queryHandler.QueryAsync(new Get<TQuery>(ids.ToImmutableList()));
+        }
+        public static Task<IImmutableList<TResponse>> GetAsync<TQuery, TResponse>(this IQuerier<Get<TQuery>, IImmutableList<TResponse>> queryHandler, IEnumerable<Id<TQuery>> ids)
+        {
+            return queryHandler.QueryAsync(new Get<TQuery>(ids.ToImmutableList()));
+        }
+
+        public static Task<IImmutableList<TResponse>> SearchAsync<TResponse>(this IQuerier<Search, IImmutableList<TResponse>> queryHandler, string text)
+        {
+            return queryHandler.QueryAsync(new Search(text));
+        }
+    }
+}
