b78f7088865afa8f5ea4680832039484029df43b
deaddog <mikkelslsl@gmail.com>
2019-04-09 23:15:13 +0200

    Queries to get/search regardless of types.

---
+++ Foosball.Domain.Application/Queries/Get.cs
@@ -0,0 +1,16 @@
+﻿using Foosball.Domain.Models;
+using System;
+using System.Collections.Immutable;
+
+namespace Foosball.Domain.Application.Queries
+{
+    public class Get<T>
+    {
+        public Get(IImmutableList<Id<T>> ids)
+        {
+            Ids = ids ?? throw new ArgumentNullException(nameof(ids));
+        }
+
+        public IImmutableList<Id<T>> Ids { get; }
+    }
+}
---
+++ Foosball.Domain.Application/Queries/Search.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Foosball.Domain.Application.Queries
+{
+    public class Search
+    {
+        public Search(string text)
+        {
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+        }
+
+        public string Text { get; }
+    }
+}
