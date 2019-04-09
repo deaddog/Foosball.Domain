b707d6e873ee0149e15fc243c87c4258b906950d
deaddog <mikkelslsl@gmail.com>
2019-04-09 20:32:25 +0200

    The most basic definition of a Player.

---
+++ Foosball.Domain.Models/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Foosball.Domain.Models
+{
+    public class Player
+    {
+        public Player(Id<Player> id)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+        }
+
+        public Id<Player> Id { get; }
+    }
+}
