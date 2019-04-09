92aa83e6a6184a71c35f1f26406e8143b68fbac6
deaddog <mikkelslsl@gmail.com>
2019-04-09 20:32:48 +0200

    A Participant descriminated union.
    Participants can be either player or team (xor).

---
+++ Foosball.Domain.Models/Participant.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foosball.Domain.Models
+{
+    public class Participant : IEquatable<Participant>
+    {
+        private readonly Id<Player> _player;
+        private readonly Team _team;
+
+        public Participant(Id<Player> player)
+        {
+            _player = player ?? throw new ArgumentNullException(nameof(player));
+            _team = null;
+        }
+        public Participant(Team team)
+        {
+            _team = team ?? throw new ArgumentNullException(nameof(team));
+            _player = null;
+        }
+
+        public bool IsPlayer => _player != null;
+        public bool IsTeam => _team != null;
+
+        public TResult Match<TResult>(Func<Id<Player>, TResult> playerSelector, Func<Team, TResult> teamSelector)
+        {
+            if (IsPlayer) return playerSelector(_player);
+            else if (IsTeam) return teamSelector(_team);
+            else throw new InvalidOperationException();
+        }
+        public void Switch(Action<Id<Player>> playerAction, Action<Team> teamAction)
+        {
+            if (IsPlayer) playerAction(_player);
+            else if (IsTeam) teamAction(_team);
+            else throw new InvalidOperationException();
+        }
+
+        public static bool operator ==(Participant participant1, Participant participant2) => EqualityComparer<Participant>.Default.Equals(participant1, participant2);
+        public static bool operator !=(Participant participant1, Participant participant2) => !(participant1 == participant2);
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Participant);
+        }
+        public bool Equals(Participant other)
+        {
+            if (IsPlayer && other.IsPlayer)
+                return _player.Equals(other._player);
+            else if (IsTeam && other.IsTeam)
+                return _team.Equals(other._team);
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Match(x => x.GetHashCode(), x => x.GetHashCode());
+        }
+
+        public override string ToString()
+        {
+            return Match(x => $"Player: {x}", x => $"Team: {x}");
+        }
+    }
+}
---
+++ Foosball.Domain.Models/Team.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Foosball.Domain.Models
+{
+    public class Team : IEquatable<Team>
+    {
+        public Team(Id<Player> player1, Id<Player> player2)
+        {
+            Player1 = player1 ?? throw new ArgumentNullException(nameof(player1));
+            Player2 = player2 ?? throw new ArgumentNullException(nameof(player2));
+        }
+
+        public Id<Player> Player1 { get; }
+        public Id<Player> Player2 { get; }
+
+        public static bool operator ==(Team team1, Team team2) => EqualityComparer<Team>.Default.Equals(team1, team2);
+        public static bool operator !=(Team team1, Team team2) => !(team1 == team2);
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Team);
+        }
+        public bool Equals(Team other)
+        {
+            return other != null &&
+                Player1.Equals(other.Player1) &&
+                Player2.Equals(other.Player2);
+        }
+
+        public override int GetHashCode()
+        {
+            return Player1.GetHashCode() ^ Player2.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{Player1}, {Player2}}}";
+        }
+    }
+}
