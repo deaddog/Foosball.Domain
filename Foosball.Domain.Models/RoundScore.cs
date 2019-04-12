fd638e51b988951caf5f22acb9b8dc899cfd5319
deaddog <mikkelslsl@gmail.com>
2019-04-12 12:42:37 +0200

    Score modeling (initial and per-round).

---
+++ Foosball.Domain.Models/RoundScore.cs
@@ -0,0 +1,15 @@
+﻿namespace Foosball.Domain.Models
+{
+    public class RoundScore : Score
+    {
+        public RoundScore(Id<Round> round, Id<Player> player, int addedPoints, int totalPoints)
+            : base(player, totalPoints)
+        {
+            Round = round ?? throw new System.ArgumentNullException(nameof(round));
+            AddedPoints = addedPoints;
+        }
+
+        public Id<Round> Round { get; }
+        public int AddedPoints { get; }
+    }
+}
---
+++ Foosball.Domain.Models/Score.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Foosball.Domain.Models
+{
+    public class Score : IComparable<Score>
+    {
+        public Score(Id<Player> player, int totalPoints)
+        {
+            Player = player ?? throw new ArgumentNullException(nameof(player));
+            TotalPoints = totalPoints;
+        }
+
+        public Id<Player> Player { get; }
+        public int TotalPoints { get; }
+
+        public int CompareTo(Score other)
+        {
+            return TotalPoints.CompareTo(other.TotalPoints);
+        }
+    }
+}
