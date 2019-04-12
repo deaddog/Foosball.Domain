bd85d45e709db91f0e91456d8a5cb68b50385308
deaddog <mikkelslsl@gmail.com>
2019-04-12 12:08:48 +0200

    Message target/receiver interfaces.

---
+++ Foosball.Domain.Application/IReceiver.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Foosball.Domain.Application
+{
+    public interface IReceiver<in TMessage>
+    {
+        Task Receive(TMessage message);
+    }
+}
---
+++ Foosball.Domain.Application/ITarget.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Foosball.Domain.Application
+{
+    public interface ITarget<in TMessage>
+    {
+        Task Post(TMessage message);
+    }
+}
