c612814f550c6a7490344f0a21077d72dd58556a
deaddog <mikkelslsl@gmail.com>
2019-06-20 22:11:16 +0200

    Added player name.

--- Foosball.Domain.Models/Player.cs
+++ Foosball.Domain.Models/Player.cs
@@ -4,11 +4,13 @@ namespace Foosball.Domain.Models
 {
     public class Player
     {
-        public Player(Id<Player> id)
+        public Player(Id<Player> id, string name)
         {
             Id = id ?? throw new ArgumentNullException(nameof(id));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
         public Id<Player> Id { get; }
+        public string Name { get; }
     }
 }
