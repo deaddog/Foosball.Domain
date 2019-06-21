3927710020a073766b76699994be0ecece69f41b
deaddog <mikkelslsl@gmail.com>
2019-06-21 10:35:13 +0200

    CreateTournament command.

---
+++ Foosball.Domain.Application/Commands/CreateTournament.cs
@@ -0,0 +1,15 @@
+﻿using Foosball.Domain.Models;
+using System;
+
+namespace Foosball.Domain.Application.Commands
+{
+    public class CreateTournament
+    {
+        public CreateTournament(Tournament tournament)
+        {
+            Tournament = tournament ?? throw new ArgumentNullException(nameof(tournament));
+        }
+
+        public Tournament Tournament { get; }
+    }
+}
