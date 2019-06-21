7b474e01e2dbe4b90955ca972d5ea6f449cd111c
AsyR <Asy-dk@live.dk>
2019-06-21 09:22:27 +0200

    Added Id<Tournament> Tournament to Round

--- Foosball.Domain.Models/Round.cs
+++ Foosball.Domain.Models/Round.cs
@@ -5,15 +5,17 @@ namespace Foosball.Domain.Models
 {
     public class Round
     {
-        public Round(Id<Round> id, IImmutableList<Id<Match>> matches)
+        public Round(Id<Round> id, IImmutableList<Id<Match>> matches, Id<Tournament> tournament)
         {
             Id = id ?? throw new ArgumentNullException(nameof(id));
-
             Matches = matches ?? throw new ArgumentNullException(nameof(matches));
+            Tournament = tournament ?? throw new ArgumentNullException(nameof(tournament));
         }
 
         public Id<Round> Id { get; }
 
         public IImmutableList<Id<Match>> Matches { get; }
+
+        public Id<Tournament> Tournament { get; }
     }
 }
